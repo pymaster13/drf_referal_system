71f04577eff293701e10797ed957ac33be7ea986
pymaster13 <root@element.localdomain>
2022-03-23 02:28:17 +0300

    release

--- referal_system/referal_system/settings.py
+++ referal_system/referal_system/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
     #     'HOST': 'localhost',
     #     'PORT': 5432,
     # },
+    
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
@@ -172,7 +173,11 @@ AUTH_USER_MODEL = 'person.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
